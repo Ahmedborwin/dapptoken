92aea15ff51bd6cca53f6ef0c9a9bdd4d2aa7a6b
Ahmed Borwin <56191075+Ahmedborwin@users.noreply.github.com>
2023-10-28 10:56:21 +0100

    add progress bar of tokens bought V total Supply

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ import Config from "./constants/config.json";
 
 function App() {
   const [provider, setProvider] = useState(null);
-
   const [accounts, setAccounts] = useState(null);
+
   const [account, setAccount] = useState(null);
-  const [treasury, setTreasury] = useState(null);
+  const [treasury, setTreasury] = useState(false);
   const [accountBalance, setAccountBalance] = useState(null);
   const [treasuryBalance, setTreasuryBalance] = useState(null);
 
@@ -26,6 +26,9 @@ function App() {
 
   const [crowdsale, setCrowdsale] = useState(null);
   const [token, setToken] = useState(null);
+
+  const [maxSupply, setMaxSupply] = useState(0);
+  const [tokensBought, setTokensBought] = useState(0);
 
   const [isLoading, setIsLoading] = useState(true);
 
@@ -62,11 +65,26 @@ function App() {
     const account = ethers.utils.getAddress(accounts[0]);
     setAccount(account);
 
+    //fetch total token Supply
+    const tokenSupply = await token.getTotalSupply();
+    setMaxSupply(tokenSupply);
+    console.log(maxSupply);
+
+    //fetch tokensBought
+    const tokensBoughtFetch = await crowdsale.getTokensSold();
+    setTokensBought(tokensBoughtFetch);
+
+    //fetch price
+    const price = await crowdsale.getTokenPrice();
+    setPrice(price);
+
     // Fetch account balance
     const accountBalance = ethers.utils.formatUnits(
       await token.balanceOf(account),
       18
     );
+    console.log(accountBalance.toString());
+
     setAccountBalance(accountBalance);
 
     // Fetch Treasury address and token balance
@@ -74,18 +92,17 @@ function App() {
     const treasuryAcc = await token.getTreasuryAddress();
     setTreasury(treasuryAcc);
 
-    const treasuryTokenBalance = ethers.utils.formatUnits(
-      await token.balanceOf(treasury),
-      18
-    );
-
-    setTreasuryBalance(treasuryTokenBalance);
-
-    //fetch price
-    const price = await crowdsale.getTokenPrice();
-    setPrice(price);
-
-    console.log("FINAL STEP");
+    await new Promise(async (resolve) => {
+      if (treasury) {
+        const treasuryTokenBalance = ethers.utils.formatUnits(
+          await token.balanceOf(treasury),
+          18
+        );
+        setTreasuryBalance(treasuryTokenBalance);
+        resolve();
+      }
+    });
+    console.log("");
 
     setIsLoading(false);
   };
@@ -99,12 +116,15 @@ function App() {
   return (
     <Container>
       <Navbar />
-      <h1>Token Vending Machine</h1>
+      <h1 className="my-4 text-center">
+        {" "}
+        <strong>Token Vending Machine</strong>
+      </h1>
       {isLoading ? (
         <Loading />
       ) : (
         <>
-          <p className="text-center">
+          <p className="text-left">
             <strong>Current Price:</strong>{" "}
             {ethers.utils.formatEther(price.toString())} ETH
           </p>
@@ -115,6 +135,10 @@ function App() {
             price={price}
             setIsLoading={setIsLoading}
             accounts={accounts}
+          />
+          <Progress
+            maxTokens={maxSupply.toString()}
+            tokenSold={tokensBought.toString()}
           />
         </>
       )}
--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,8 +1,16 @@
+import ProgressBar from "react-bootstrap/ProgressBar";
+import { ethers } from "ethers";
+
 const Progress = ({ maxTokens, tokenSold }) => {
   return (
     <div className="my-3">
+      <ProgressBar
+        now={(tokenSold / maxTokens) * 100}
+        label={`${(tokenSold / maxTokens) * 100}%`}
+      />
       <p className="text-center my-3">
-        Total Tokens: {maxTokens} / Tokens Sold: {tokenSold}{" "}
+        Total Tokens: {ethers.utils.formatEther(maxTokens).toString()} / Tokens
+        Sold: {ethers.utils.formatEther(tokenSold).toString()}
       </p>
     </div>
   );
